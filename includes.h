7c91eac9af93c06075acfbbca789cbbb0907fa7f
falue <info@fluescher.ch>
2020-11-25 19:53:52 +0100

    split code into different files

---
+++ includes.h
@@ -0,0 +1,6 @@
+// LIBRARIES ----------------------------------------------------------------------------------
+#include <MD_REncoder.h>
+#include <ArduinoSort.h>
+#include <SPI.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_HX8357.h"
